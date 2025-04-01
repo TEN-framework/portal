ecf328f0eba9c31ae6dd5e8f69de55902dfb0d18
czhen <56986964+shczhen@users.noreply.github.com>
2025-04-01 15:13:48 +0800

    fix: icons

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -9,16 +9,25 @@ export function baseOptions(locale: string): BaseLayoutProps {
       title: (
         <>
           <svg
+            xmlns="http://www.w3.org/2000/svg"
+            id="_图层_2"
+            data-name="图层_2"
+            viewBox="0 0 159.66 114.89"
             width="24"
             height="24"
-            xmlns="http://www.w3.org/2000/svg"
             aria-label="Logo"
+            className="text-primary"
           >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
+            <g id="_图层_1-2" data-name="图层_1">
+              <g id="logo-logo" fill="currentColor">
+                <polygon points="135.36 2.41 135.53 2.41 133.07 .18 130.24 .17 130.24 .17 30.23 0 29.52 2.05 0 87.79 24.77 114.89 56.09 114.83 87.42 23.84 54.35 23.62 32.34 2.06 132.54 2.4 156.92 25.93 118.28 24.55 87.2 114.83 129.23 114.4 159.66 26.03 135.36 2.41" />
+              </g>
+            </g>
           </svg>
-          TEN Framework
+          TEN
         </>
       ),
+      url: `/${locale}`,
     },
     links: [
       {
