ac21c6bc478a143fe6595aac477eacbfa036cbd2
Elliott Chen <cyfyifanchen@gmail.com>
2025-04-02 19:12:57 +0800

    feat: update home and blog page styles
    - Changed the "Coming Soon" header to a larger font size and updated its text to uppercase for better visibility.
    - Enhanced the blog page layout with a new gradient background and improved structure for better user experience.
    - Added author and publication date information to blog posts for better context.

--- app/[lang]/(home)/page.tsx
+++ app/[lang]/(home)/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Comming Soon</h1>
+      <h1 className="mb-4 text-8xl font-bold">COMING SOON</h1>
     </main>
   )
 }
