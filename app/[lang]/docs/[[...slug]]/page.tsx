717190e2025771ee57c07614f39c6c45a9fa06ac
czhen <56986964+shczhen@users.noreply.github.com>
2025-04-03 11:57:33 +0800

    fix: style

--- app/[lang]/docs/[[...slug]]/page.tsx
+++ app/[lang]/docs/[[...slug]]/page.tsx
@@ -20,7 +20,19 @@ export default async function Page(props: {
   const MDXContent = page.data.body
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage
+      toc={page.data.toc}
+      full={page.data.full}
+      tableOfContent={{
+        style: 'clerk',
+      }}
+      editOnGithub={{
+        owner: 'TEN-framework',
+        repo: 'portal',
+        sha: 'main',
+        path: `content/docs/${page.file.path}`,
+      }}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
