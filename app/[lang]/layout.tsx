dbcc62a3c23591a040056f6dfde2963e1b84b00c
Elliot Chen <cyfyifanchen@gmail.com>
2025-04-25 15:07:57 +0800

    feat: add image with referrer policy to layout component

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -62,6 +62,7 @@ export default async function Layout({
             {children}
           </RootProvider>
         </NextIntlClientProvider>
+        <img referrerPolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=1295f95b-eaaa-4f5c-8861-825c0e350978" />
       </body>
     </html>
   )
