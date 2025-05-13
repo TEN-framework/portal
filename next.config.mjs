59fcd26c7721d12c1e0dc4586a575f1f028e6ec7
Elliot Chen <2340896+cyfyifanchen@users.noreply.github.com>
2025-05-13 16:46:27 +0800

    feat: adding S3 as image domain in next config (#33)

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const withNextIntl = createNextIntlPlugin('./lib/next-intl-requests.ts')
 /** @type {import('next').NextConfig} */
 const config = {
   turbopack: true,
+  images: {
+    domains: ['ten-framework-assets.s3.amazonaws.com'],
+  },
 }
 
 export default withNextIntl(withMDX(config))
