5755d27a755f1968e9f566e78c54d59d27c873e5
Elliot Chen <cyfyifanchen@gmail.com>
2025-06-02 10:36:42 +0800

    fix: fixing the text being cut off on mobile device

--- constants/url.ts
+++ constants/url.ts
@@ -2,4 +2,5 @@ export const URL_TEN_AGENT = 'https://agent.theten.ai'
 export const BLOG = '/blog'
 export const URL_TEN_FRAMEWORK_DOC = '/docs/ten_framework/concept_overview'
 export const HUGGING_FACE_SPACE = 'https://huggingface.co/TEN-framework'
-export const URL_TEN_TURN_DETECTION = 'https://github.com/ten-framework/ten-turn-detection'
+export const URL_TEN_TURN_DETECTION =
+  'https://github.com/ten-framework/ten-turn-detection'
