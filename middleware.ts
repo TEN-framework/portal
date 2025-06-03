b4d7bf4a0b83ddc2a1839227080d07f9c67a2a0d
Elliot Chen <2340896+cyfyifanchen@users.noreply.github.com>
2025-06-03 15:52:15 +0800

    fix: adding the right woff2 to the middleware and removing unnecessary font (#57)

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ import { i18n } from '@/lib/i18n'
 export default createI18nMiddleware(i18n)
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:js|css|png|jpg|jpeg|gif|svg|webp|ico)).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:js|css|png|jpg|jpeg|gif|svg|webp|ico|woff2)).*)'],
 }
